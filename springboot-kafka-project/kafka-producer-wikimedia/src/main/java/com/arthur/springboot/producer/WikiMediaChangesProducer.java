93354280f1c0a517f20d820a81179484fa862edd
Kaung Htut Hlaing <kaunghtut18@gmail.com>
2026-01-15 14:28:04 +0700

    implemented producer

---
+++ springboot-kafka-project/kafka-producer-wikimedia/src/main/java/com/arthur/springboot/producer/WikiMediaChangesProducer.java
@@ -0,0 +1,46 @@
+package com.arthur.springboot.producer;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import com.arthur.springboot.handler.WikimediaChangesHandler;
+import okhttp3.Headers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class WikiMediaChangesProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikiMediaChangesProducer.class);
+    private KafkaTemplate <String, String> kafkaTemplate;
+
+    public WikiMediaChangesProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage() throws InterruptedException {
+
+        //To read real time stream data from Wikimedia and send to Kafka topic we use event source
+        EventHandler eventHandler = new WikimediaChangesHandler(kafkaTemplate, "wikimedia_recentchange");
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+
+//        Map<String, String> headers = new HashMap<>();
+//        headers.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
+//        headers.put("Accept", "text/event-stream");
+        Headers headers = new Headers.Builder()
+                .add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+                .add("Accept", "text/event-stream")
+                .build();
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url))
+                .headers(headers);
+        EventSource eventSource = builder.build();
+        eventSource.start();
+
+        TimeUnit.MINUTES.sleep(10);
+    }
+}
